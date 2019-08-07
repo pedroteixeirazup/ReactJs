c7c3b83faeca185f832fd41403aee29ad04ce0b2
Pedro <pedroteixeira@zup.com>
2019-08-07 12:43:15 -0300

    Bug at Longin.js/Login.css

---
+++ omnistack/frontend/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+import './Login.css';
+
+
+export default function Login() {
+  return (
+    <div className="login-container">
+        <img src={logo} alt="Tindev"/>
+    </div>
+  );
+}
+
