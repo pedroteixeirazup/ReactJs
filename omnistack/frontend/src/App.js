ff5813164165629151420d6daf4773e1fa3bfd23
Pedro <pedroteixeira@zup.com>
2019-08-08 10:23:12 -0300

    Frontend omnistack week

--- omnistack/frontend/src/App.js
+++ omnistack/frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import Login from './pages/Login';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <>
+      <Routes />
+    </>
   );
 }
 
--- omnistack/frontend/src/pages/Login.js
+++ omnistack/frontend/src/pages/Login.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo.svg';
 import './Login.css';
+import api from '../services/api';
 
 
-export default function Login() {
+export default function Login({ history }) {
+  const [username, setUsername] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      username: username,
+    });
+
+    const { _id } = response.data;
+
+    history.push(`/dev/${ _id }`);
+  }
+
   return (
     <div className="login-container">
-        <img src={logo} alt="Tindev"/>
+        <form onSubmit={handleSubmit}>
+            <img src={logo} alt="Tindev"/>
+            <input 
+                placeholder="Digite seu usuário no Github"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+            />
+            <button type="submit">Enviar</button>
+        </form>
     </div>
   );
 }
